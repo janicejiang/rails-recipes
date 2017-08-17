193babcc905e4bf46a8281902aae5ab3654d28a8
janice <janicejiang.d@gmail.com>
2017-08-17 16:14:07 +0800

    10. 嵌套表单(1-to-many)

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'devise'
 gem "devise-i18n"
 gem "rails-i18n"
 gem "select2-rails"
+gem "nested_form_fields"
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -10,6 +10,8 @@ class Admin::EventsController < AdminController
 
   def new
     @event = Event.new
+    @event.tickets.build
+    # @event.tickets.build
   end
 
   def create
@@ -24,6 +26,9 @@ class Admin::EventsController < AdminController
 
   def edit
     @event = Event.find_by_friendly_id!(params[:id])
+    # @event.tickets.build
+    # @event.tickets.build
+    @event.tickets.build if @event.tickets.empty?
   end
 
   def update
@@ -46,6 +51,6 @@ class Admin::EventsController < AdminController
   protected
 
   def event_params
-    params.require(:event).permit(:name, :description, :friendly_id, :status, :category_id)
+    params.require(:event).permit(:name, :description, :friendly_id, :status, :category_id, :tickets_attributes => [:id, :name, :description, :price, :_destroy])
   end
 end
